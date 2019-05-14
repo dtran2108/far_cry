9ee7722a4b0a95a0e9201ce22534f80c9a796a9d
Trân Dương <dtran2108@gmail.com>
2019-05-15 01:55:45 +0700

    wp4 completed

--- main.py
+++ main.py
@@ -67,5 +67,28 @@ def parse_log_start_time(log_data):
     return datetime_obj
 
 
+def parse_session_mode_and_map(log_data):
+    """
+    return a tuple (mode, map) where:
+        - mode: indicates the multiplayer mode that was played
+        - map: the name of the map that was player
+
+    @param:
+    log_data: the data read from a Far Cry server's log file
+    """
+    temp = ''
+    for line in log_data.split('\n'):
+        if "Loading level" in line:
+            for word in line[8:]:
+                if word != '-':
+                    temp += word
+            break
+    temp = temp.split(',')
+    game_map = temp[0][temp[0].index('/')+1:]
+    game_mode = temp[1].split('mission ')[1]
+    return (game_mode, game_map)
+
+
 if __name__ == '__main__':
     start_time = parse_log_start_time(read_log_file('./logs/log06.txt'))
+    parse_session_mode_and_map(read_log_file('./logs/log00.txt'))
