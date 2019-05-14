a17c726b820ac1df260dedf1d12d88cfd4a5f723
Trân Dương <dtran2108@gmail.com>
2019-05-15 04:14:01 +0700

    wp 5 completed

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def read_log_file(log_file_path):
     return log data read from log file
 
     @param
-    log_file_path: path to the log file
+    log_file_path: path-like object giving the pathname
+                   (absolute or relative to the current
+                   working directory) of the log file
     """
     with open(log_file_path) as f:
         log_data = f.read()
@@ -67,6 +69,53 @@ def parse_log_start_time(log_data):
     return datetime_obj
 
 
+def get_frag_lines(log_data):
+    """
+    return a list contains lines of frag history
+
+    @param
+    log_data: the data read from a Far Cry server's log file
+    """
+    frag_line_list = []
+    for line in log_data.split('\n'):
+        if 'killed' in line:
+            frag_line_list.append(line)
+    return frag_line_list
+
+
+def get_frag(frag_line):
+    """
+    return a tuple of the following form:
+        (frag_time, killer_name, victim_name, weapon_code)
+    or, the simple following form, if the player committed suicide:
+        (frag_time, killer_name)
+
+    @param
+    frag_line: a single line of frag, for example,
+               <47:26> <Lua> cyap killed papazark with SniperRifle
+    """
+    elements = frag_line.split()
+    if len(elements) == 5:
+        return (elements[0][1:-1], elements[2])
+    else:
+        return (elements[0][1:-1], elements[2], elements[4], elements[-1])
+
+
+def parse_frags(log_data):
+    """
+    return a list of frags
+
+    @param
+    log_data: the data read from a Far Cry server's log file
+    """
+    frag_lines = get_frag_lines(log_data)
+    frag_list = []
+    for line in frag_lines:
+        frag_list.append(get_frag(line))
+    return frag_list
+
+
+
 def parse_session_mode_and_map(log_data):
     """
     return a tuple (mode, map) where:
@@ -90,5 +139,4 @@ def parse_session_mode_and_map(log_data):
 
 
 if __name__ == '__main__':
-    start_time = parse_log_start_time(read_log_file('./logs/log06.txt'))
-    parse_session_mode_and_map(read_log_file('./logs/log00.txt'))
+    print(parse_frags(read_log_file('./logs/log00.txt')))
