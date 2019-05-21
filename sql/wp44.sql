35b01534072551bc6e8af86b9a043dc700286a21
dtran00 <dtran2108@gmail.com>
2019-05-21 22:59:12 +0700

    45 in progress

---
+++ sql/wp44.sql
@@ -0,0 +1,24 @@
+SELECT match_id,
+       player_name,
+       Sum(kill_count) as kill_count,
+       sum(sucide_count) as sucide_count,
+       sum(death_count) as death_count,
+       100.0 * sum(kill_count) / (sum(kill_count) + sum(death_count) + sum(sucide_count)) as efficiency
+FROM 
+    (select match_id,
+            killer_name as player_name,
+            count(killer_name) as kill_count,
+            (count(*) - count(victim_name)) as sucide_count,
+            0 as death_count
+    from match_frag
+    group by killer_name
+    union
+    select match_id,
+           victim_name as player_name,
+           0 as kill_count,
+           0 as sucide_count,
+           count(victim_name) as death_count 
+    from match_frag
+    where victim_name is not null
+    group by player_name)
+GROUP BY match_id, player_name
