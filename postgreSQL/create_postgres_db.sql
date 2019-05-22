1f3d728fd9b46b89c9878a2b09c97c91112d2c8e
Ngọc Quỳnh <dafj@gmail.com>
2019-05-22 23:23:38 +0700

    create postgres database

---
+++ postgreSQL/create_postgres_db.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE farcry ENCODING 'utf-8';
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS match
+(
+    match_id   uuid           NOT NULL DEFAULT uuid_generate_v1(),
+    start_time timestamptz(3) NOT NULL,
+    end_time   timestamptz(3) NOT NULL,
+    game_mode  text           NOT NULL,
+    map_name   text           NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS match_frags
+(
+    match_id    uuid        NOT NULL,
+    frag_time   timestamptz NOT NULL,
+    killer_name text        NOT NULL,
+    victim_name text,
+    weapon_code text
+);
+
+ALTER TABLE match
+    ADD CONSTRAINT pk_match_match_id
+        PRIMARY KEY (match_id);
+
+ALTER TABLE match_frags
+    ADD CONSTRAINT fk_match_frag_match_id 
+        FOREIGN KEY (match_id) REFERENCES match (match_id) 
+            ON UPDATE CASCADE ON DELETE RESTRICT;
