a58fe84f7c79902959c81f6a3658b2e2f39f5c17
Ngọc Quỳnh <dafj@gmail.com>
2019-05-22 23:28:53 +0700

    add comments

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ from sql_modules.insert import insert_match_to_sqlite
 
 
 def main():
+    # get all the neccessary data
     log_data = read_log_file('./logs/log00.txt')
     mode_game, map_game = parse_session_mode_and_map(log_data)
     start_time = parse_log_start_time(log_data)
     game_start_time, game_end_time = parse_game_session_start_and_end_time(log_data, map_game, start_time)
     frags = parse_frags(log_data)
-    # insert_match_to_sqlite('./farcry.db', game_start_time, game_end_time, mode_game, map_game, frags)
+    # export data to csv file
     write_frag_csv_file('./csv_files/log00.csv', frags)
+    # insert data to sql database
+    insert_match_to_sqlite('./farcry.db', game_start_time, game_end_time, mode_game, map_game, frags)
 
 
 if __name__ == '__main__':
